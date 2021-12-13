4430a3dc95227ac07819ec33b7d2d4e1acf677eb
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-12-13 12:08:22 +0100

    before seeding

---
+++ db/schema.rb
@@ -0,0 +1,77 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_13_105104) do
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "departure_date"
+    t.date "arrival_date"
+    t.integer "departure_time"
+    t.integer "arrival_time"
+    t.integer "user_id", null: false
+    t.integer "car_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.integer "price_per_day"
+    t.string "rental_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "drop_offs", force: :cascade do |t|
+    t.integer "location_id", null: false
+    t.integer "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_drop_offs_on_booking_id"
+    t.index ["location_id"], name: "index_drop_offs_on_location_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "agency_name"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pick_ups", force: :cascade do |t|
+    t.integer "location_id", null: false
+    t.integer "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_pick_ups_on_booking_id"
+    t.index ["location_id"], name: "index_pick_ups_on_location_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.date "date_of_birth"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "drop_offs", "bookings"
+  add_foreign_key "drop_offs", "locations"
+  add_foreign_key "pick_ups", "bookings"
+  add_foreign_key "pick_ups", "locations"
+end
